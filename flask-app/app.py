833a0f51a3d7c2632bf12a65cc225191ae57f69b
Claus Krag <ckra@tv2.dk>
2018-11-07 10:57:58 +0100

    added basic auth

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,10 +4,17 @@ from flask import Flask
 from flask import abort
 from flask import request
 from flask import make_response
+from flask_basicauth import BasicAuth
+
 
 from model.dms import DMS as DMS_APPLICATION
 
 app = Flask(__name__)
+
+# https://flask-basicauth.readthedocs.io/en/latest/
+app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = 'Secret123'
+basic_auth = BasicAuth(app)
 
 # https://medium.com/@shamir.stav_83310/dockerizing-a-flask-mysql-app-with-docker-compose-c4f51d20b40d
 # http://flask.pocoo.org/docs/1.0/quickstart/#about-responses
@@ -19,6 +26,7 @@ DMS = DMS_APPLICATION()
 
 
 @app.route('/app/<string:app_id>', methods=['POST'])
+@basic_auth.required
 def receive_data(app_id: str) -> str or int:
     app_id = app_id.lower()
 
@@ -31,6 +39,7 @@ def receive_data(app_id: str) -> str or int:
 
 
 @app.route('/app/<string:app_id>', methods=['DELETE'])
+@basic_auth.required
 def delete_app(app_id: str) -> str or int:
     app_id = app_id.lower()
 
@@ -39,6 +48,7 @@ def delete_app(app_id: str) -> str or int:
 
 
 @app.route('/app/<string:app_id>', methods=['GET'])
+@basic_auth.required
 def show_app(app_id: str) -> str:
 
     app_data = DMS.on_app_requested(app_id)
@@ -53,6 +63,7 @@ def show_app(app_id: str) -> str:
 
 
 @app.route('/entries/<string:app_id>', methods=['GET'])
+@basic_auth.required
 def show_entries(app_id: str):
     app_id = app_id.lower()
 
@@ -63,6 +74,7 @@ def show_entries(app_id: str):
 
 
 @app.route('/apps', methods=['GET'])
+@basic_auth.required
 def show_apps():
     resp = make_response(DMS.on_apps_requested())
     resp.headers['Content-Type'] = 'application/json'
