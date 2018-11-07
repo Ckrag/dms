b9911f722b86179a8d3046d50997a68c752957dd
Claus Krag <ckra@tv2.dk>
2018-11-07 21:05:09 +0100

    removed django

--- admin_site/overview/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:app_id>/', views.detail, name='detail'),
-    path('update/', views.save_app_config, name='save_app_config')
-]
