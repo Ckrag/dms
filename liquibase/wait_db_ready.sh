459efa6e52eb88fe05ab31e2ea0940ad03963498
Claus Freilev Krag <cfk@cardlay.com>
2019-11-17 20:33:21 +0100

    updated compose to also launch app, cleared some debug commentry

--- liquibase/wait_db_ready.sh
+++ liquibase/wait_db_ready.sh
@@ -1,16 +1,8 @@
 DB_HOST="flask_db"
 DB_PORT="5432"
 
-#while !</dev/tcp/db/5432; do sleep 1; done;
-
-#while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done;
-# Almost done..
-#sleep 10;
-
-
-
 while ! pg_isready -h ${DB_HOST} -p ${DB_PORT} > /dev/null 2> /dev/null; do
-    echo "Connecting to ${DB_HOST} Failed"
+    echo "Pre-liquid-test: Connecting to ${DB_HOST} Failed"
     sleep 1
   done
 
